a615a11bc94c46ffb1f5e4d206955e945a3386f6
Pedro Serer <pedroserer@gmail.com>
2023-10-24 21:22:00 -0300

    Primeiro Commit

---
+++ EntradaSaida/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualBasic;
+
+namespace EntradaSaida {
+    internal class Program {
+        static void Main(string[] args) {
+            // Vetor com as perguntas
+            string[] dadosPessoa = new string[5] {
+                "Digite seu nome: ",
+                "Digite sua profissão: ",
+                "Digite seu telefone; ",  
+                "Digite seu email: ",
+                "Digite sua frase do coração: "
+            };
+
+            // Vetor com as respostas da pergunta
+            string[]? valoresDados = new string[5];
+
+            // Preenche o vetor com dados
+            for (int i = 0; i < 5; i++) {
+                Console.WriteLine(dadosPessoa[i]);
+                valoresDados[i] = Console.ReadLine();
+                Console.WriteLine("\n");
+                
+            }
+
+            // Apresenta os dados na tela
+            Console.WriteLine("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+            for (int i = 0; i < 5; i++) {
+                Console.WriteLine(formataStrings("x    " + valoresDados[i]));
+            }
+            Console.WriteLine("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+        }
+
+        string formataStrings( (string mensagem))
+        {
+            return '';
+        }
+    }
+}
